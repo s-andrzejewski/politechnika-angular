df89f6beeda43277dc577577d13e4e44c4a73291
s-andrzejewski <113853833+s-andrzejewski@users.noreply.github.com>
2022-10-26 12:49:33 +0200

    dodano button z funkcją, ale jest tutaj błąd, nie wyświetla się button

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
+  task: string = '';
+  taskList: string[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onButtonPress() {
+    this.taskList.push(this.task);
   }
-
 }
