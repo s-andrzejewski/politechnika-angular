34d4de7b993343b15f9345fee4959d6975944a33
s-andrzejewski <113853833+s-andrzejewski@users.noreply.github.com>
2022-10-26 12:38:52 +0200

    wyłaczenie poprzednich komponentów, dodanie komponentu todo listy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AlertWarningComponent } from './alert-warning/alert-warning.component';
 import { AlertInfoComponent } from './alert-info/alert-info.component';
 import { BindingTaskComponent } from './binding-task/binding-task.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { BindingTaskComponent } from './binding-task/binding-task.component';
     AlertWarningComponent,
     AlertInfoComponent,
     BindingTaskComponent,
+    TodoListComponent,
   ],
   imports: [BrowserModule, NgbModule],
   providers: [],
